3639d9349261c2d56030f81c3ec8562eb3185cfb
medbrat13 <medbrat1994@gmail.com>
2019-07-29 03:33:29 +0300

    Добавил порт в конфиг

--- config/db_conf.php
+++ config/db_conf.php
@@ -1,15 +1,15 @@
 <?php
-
 /**
  * Настройки подключения к базе данных
  */
 
 return [
     'driver' => 'pgsql',
-    'host' => 'localhost',
+    'host' => '127.0.0.1',
+    'port' => '5432',
     'dbname' => 'jsyf',
-    'user' => 'user_name',
-    'pass' => 'your_password',
+    'user' => 'user',
+    'pass' => 'pass',
     'opt'  => [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
