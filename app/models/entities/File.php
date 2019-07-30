2c2ef765a7a4112a68884d65deb54e0f17929789
medbrat13 <medbrat1994@gmail.com>
2019-07-30 06:56:30 +0300

    Описал сущность Файл

--- app/models/entities/File.php
+++ app/models/entities/File.php
@@ -7,5 +7,173 @@ namespace JSYF\App\Models\Entities;
  */
 class File
 {
+    private $id;
 
+    private $name;
+
+    private $album;
+
+    private $date;
+
+    private $size;
+
+    private $resolution;
+
+    private $duration;
+
+    private $comment;
+
+    private $path;
+
+    private $previewPath;
+
+    private $user;
+
+    private $ext;
+
+
+    public function __construct($name, $album, $size, $resolution = null, $duration = null, $comment = null, $path = null, $previewPath = null, $user = null, $ext = null, $id = null, $date = null)
+    {
+
+        $this->id = $id;
+        $this->name = $name;
+        $this->album = $album;
+        $this->date = $date;
+        $this->size = $size;
+        $this->resolution = $resolution;
+        $this->duration = $duration;
+        $this->comment = $comment;
+        $this->path = $path;
+        $this->previewPath = $previewPath;
+        $this->user = $user;
+        $this->ext = $ext;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAlbum()
+    {
+        return $this->album;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getResolution()
+    {
+        return $this->resolution;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date): void
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @param mixed $size
+     */
+    public function setSize($size): void
+    {
+        $this->size = $size;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPreviewPath()
+    {
+        return $this->previewPath;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getExt()
+    {
+        return $this->ext;
+    }
 }
